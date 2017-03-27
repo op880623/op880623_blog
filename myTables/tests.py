fa14243711fd0c0eae89823bbc06aeca80901eba
op880623 <op880623@gmail.com>
2017-03-27 17:36:52 +0800

    table modified

--- myTables/tests.py
+++ myTables/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Champion
+
+
+class ChampionViewTests(TestCase):
+    def test_info_view_with_no_champion(self):
+#        response = self.client.get(reverse('lol_champion_info:'))
+        response = self.client.get('/lol_champion_info')
+        self.assertEqual(response.status_code, 301)
+#        self.assertContains(response, "All champions seem to have a vacation together!", status_code=301)
+        self.assertContains(response, "500", status_code=301)
+#        self.assertQuerysetEqual(response.context['table'], [])
--- op880623_blog/urls.py
+++ op880623_blog/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^myTables/', include('myTables.urls')),
+    url(r'^lol_champion_info/', include('myTables.urls')),
     url(r'^admin/', admin.site.urls),
 ]
