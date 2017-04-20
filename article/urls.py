dd836bead9ae268c9b7c9c42e2b93ae47e6dd637
op880623 <op880623@gmail.com>
2017-04-20 16:30:37 +0800

    add bootstrap templates

--- article/urls.py
+++ article/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='article list'),
+    # url(r'^article/$', views.article_content),
+    url(r'^(?P<id>[0-9]+)/$', views.article_content),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Article
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("You're looking at question.")
+
+
+def article_content(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, 'startbootstrap-blog-4-dev/post.html')
