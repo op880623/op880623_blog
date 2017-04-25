2c8528283e5ebb62af439b578a841cc15cd911c9
op880623 <op880623@gmail.com>
2017-04-25 16:50:44 +0800

    templates modified.

--- article/views.py
+++ article/views.py
@@ -10,7 +10,7 @@ def list(request, page='1'):
 def list_by_category(request, category=''):
     classification = Classification.objects.get(name=category)
     articles = get_list_or_404(Article, classification=classification)
-    return render(request, 'startbootstrap-blog-4-dev/post.html')
+    return render(request, 'startbootstrap-blog-4-dev/article.html')
 
 def article_by_id(request, id):
     article = get_object_or_404(Article, id=int(id))
@@ -19,6 +19,6 @@ def article_by_id(request, id):
 def article_by_category_id_slug(request, id, slug='', category=''):
     article = get_object_or_404(Article, id=int(id))
     if slug == article.slug() and id == str(int(id)).zfill(5) and category == article.classification.format_url()[:-1]:
-        return render(request, 'startbootstrap-blog-4-dev/post.html')
+        return render(request, 'startbootstrap-blog-4-dev/article.html',  {'article': article})
     else:
         return redirect(reverse('url_with_id', kwargs={'id': str(int(id)).zfill(5)}))
